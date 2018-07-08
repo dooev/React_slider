c9a8f4ba2ace6b27f93dd32713f74960cdbf39fb
Dima <dooevdima3@gmail.com>
2018-07-08 11:26:32 +0300

    Home work

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ function Hellow() {
 function SayFullName(props) {
 	return (
 					<div>
-						<h1>Моё имя {props.name}, фамилия - {props.surname}</h1>
-						<a href={props.link}> Ссылка на Vk</a>
+						<h1>Моё имя {props.name}, и я говорю вам - {props.say}</h1>
+						<a href={props.link}> Ссылка на выход в Мир</a>
 					</div>
 		)
 }
@@ -24,9 +24,7 @@ function All(){
 	return (
 					<div>
 
-					<SayFullName name='Du' link='ya.ru'/>
-					<SayFullName name='Fd' link='yafd.ru'/>
-					<SayFullName name='Dffff' link='yaf.ru'/>
+					<SayFullName name='Dima' say='Hellow World!'link='ya.ru'/>
 					</div>
 	
 		)
