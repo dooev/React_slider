f1cb51172c3d7466a004473cddbb490fe697a8b5
Dima <dooevdima3@gmail.com>
2018-07-08 22:09:40 +0300

    Home work day 2

--- src/index.js
+++ src/index.js
@@ -4,32 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-function Hellow() {
-	let world = "World"
-	return (
-					<h1> Hellow {world} </h1>
-		)
-}
 
-function SayFullName(props) {
-	return (
-					<div>
-						<h1>Моё имя {props.name}, и я говорю вам - {props.say}</h1>
-						<a href={props.link}> Ссылка на выход в Мир</a>
-					</div>
-		)
-}
-
-function All(){
-	return (
-					<div>
-
-					<SayFullName name='Dima' say='Hellow World!'link='ya.ru'/>
-					</div>
-	
-		)
-}
-
-
-ReactDOM.render(<All />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
